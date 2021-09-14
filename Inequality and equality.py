4bf1d32c6a6119d14dda24b8b13b86b45cd279e0
ChaitaliKPatil <90540795+ChaitaliKPatil@users.noreply.github.com>
2021-09-14 21:36:30 +0530

    Create Inequality and equality.py

---
+++ Inequality and equality.py
@@ -0,0 +1,18 @@
+#Q4
+#Comaperitive test of two numbers or texts using equality and inequality operators
+nil=0
+num=0
+max=1
+two=2
+maxx=2
+cap='A'
+low='a'
+print('Q4: Comparing values\n1.nil =',nil,'\tnum=',num,'\nEQUALITY TEST\n',nil,'==',num,nil==num,end='\n') #Equality check True
+print('2.cap =',cap,'\tlow=',low,'\nEQUALITY TEST\n',cap,'==',low,cap==low,end='\n') #Equality check False
+print('3.cap =',cap,'\tlow=',low,'\nINEQUALITY TEST\n',cap,'!=',low,cap!=low,end='\n') #Inequality check True
+print('4.nil =',nil,'\tmax=',max,'\nGREATER THAN TEST\n',nil,'>',max,nil>max,end='\n') #Greater than check 
+print('5.nil =',nil,'\tmax=',max,'\nLESS THAN TEST\n',nil,'<',max,nil<max,end='\n') #less than check 
+print('6.nil =',nil,'\ttwo =',two,'\tmaxx=',maxx,'\nLESS THAN OR EQUAL TO TEST\n',nil,'<=',maxx,nil<=maxx,'\n',two,'<=',maxx,two<=maxx) #less than or equal to check 
+print('7.nil =',nil,'\tmax=',max,'\ttwo =',two,'\nGREATER THAN OR EQUAL TO TEST\n',nil,'>=',max,nil>=max,'\n',two,'>=',max,two>=max)
+#Greater than or equal to check 
+print('\n\n\n\n')
